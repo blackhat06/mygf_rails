f4cf89dd40ae8928751dc0dabd3d6a425c507761
root <root@py.(none)>
2013-05-08 15:07:03 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Semblance::Application.config.secret_token = '0d4382a7988d074e7f2654848755b72c163de132dab52c1e3e8ed4c539a9a627b07685a9ba80c3dd466a76c9666904c238d70ab7e5a0bdf99525af2e7ff34373'
